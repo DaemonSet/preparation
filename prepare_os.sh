1bbe4f3f1a9022335ef30bc6b942364d3d327413
Sergey Kulygin <s.v.kulygin@gmail.com>
2023-04-16 23:36:52 +0700

    Update prepare_os.sh

--- prepare_os.sh
+++ prepare_os.sh
@@ -6,3 +6,9 @@ read userPass
 
 echo $userPass | sudo -S bash -c "echo '$USER ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/$USER"
 
+sudo timedatectl set-timezone Asia/Novosibirsk
+
+date
+
+sed -i '/force_color_prompt=yes/s/#//' $HOME/.bashrc
+source $HOME/.bashrc
